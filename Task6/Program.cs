1990d110f1c0a4a506f0f3596de64f15d1dfcad7
Sergey <neowaps1@mail.ru>
2023-04-14 18:28:39 +0300

    completed Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+
+double even = number % 2;
+if (even == 0)
+{
+    System.Console.WriteLine("Число " + number + " является чётным");
+}
+else
+{
+    System.Console.WriteLine("Число " + number + " является нечётным");
+}
