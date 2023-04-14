c4713630440c3e6d9bac400dff96ab6916b30c90
Sergey <neowaps1@mail.ru>
2023-04-14 17:39:43 +0300

    completed Task2 and Task4

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите первое число: ");
+string textA = Console.ReadLine();
+int numberA = Convert.ToInt32(textA);
+System.Console.Write("Введите второе число: ");
+string textB = Console.ReadLine();
+int numberB = Convert.ToInt32(textB);
+
+if (numberA > numberB)
+{
+    System.Console.WriteLine("Число " + numberA + " больше, чем число " + numberB);
+}
+else
+{
+    System.Console.WriteLine("Число " + numberB + " больше, чем число " + numberA);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.Write("Введите первое число: ");
+string textA = Console.ReadLine();
+int numberA = Convert.ToInt32(textA);
+System.Console.Write("Введите второе число: ");
+string textB = Console.ReadLine();
+int numberB = Convert.ToInt32(textB);
+System.Console.Write("Введите третье число: ");
+string textC = Console.ReadLine();
+int numberC = Convert.ToInt32(textC);
+int max = Convert.ToInt32(textA);
+
+
+if (numberA >= numberB & numberA >= numberC)
+{
+    max = numberA;
+}
+if (numberB >= numberA & numberB >= numberC)
+{
+    max = numberB;
+}
+if (numberC >= numberA & numberC >= numberB)
+{
+    max = numberC;
+}
+System.Console.WriteLine("Максимальным из предоставленных чисел является: " + max);
